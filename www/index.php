c6f306e9399a3697cedfcea00403f5624d3b8743
sakermaker <jesus.suarez.ledesma@hotmail.com>
2025-04-05 23:46:36 +0200

    Exercises and Readme

--- www/index.php
+++ www/index.php
@@ -1,75 +1,49 @@
+<!-- INDEX FILE FOR EXERCISE 2
+ 
+    Please note, all files inside controller, model and views folders are related to this exercise. The only folder which isn't related to this project is exercise1.
+
+    Exercise summary:
+    I've replicated an MVC structure for this exercise. Controller folder contains the DNI checks and the appointment reservation calls. Further explanation in each file.
+    Model folder contains the Appointment class with its respective CheckDNI and CheckLastHour functions. Further explanation in the file comments.
+    Last but not least, the views folder contains the styles and ajax script. Further explanation in the file comments.
+
+    This index file contains the form.
+    
+-->
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAMP STACK</title>
-        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
-        <link rel="stylesheet" href="/assets/css/bulma.min.css">
-    </head>
-    <body>
-        <section class="hero is-medium is-info is-bold">
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <h1 class="title">
-                        LAMP STACK
-                    </h1>
-                    <h2 class="subtitle">
-                        Your local development environment
-                    </h2>
-                </div>
-            </div>
-        </section>
-        <section class="section">
-            <div class="container">
-                <div class="columns">
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Environment</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><?= apache_get_version(); ?></li>
-                                <li>PHP <?= phpversion(); ?></li>
-                                <li>
-                                    <?php
-                                    $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
-
-/* check connection */
-                                    if (mysqli_connect_errno()) {
-                                        printf("MySQL connecttion failed: %s", mysqli_connect_error());
-                                    } else {
-                                        /* print server version */
-                                        printf("MySQL Server %s", mysqli_get_server_info($link));
-                                    }
-                                    /* close connection */
-                                    mysqli_close($link);
-                                    ?>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Quick Links</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><a href="/phpinfo.php">phpinfo()</a></li>
-                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
-                                <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
-                                <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <footer class="footer">
-            <div class="content has-text-centered">
-                <p>
-                    <strong><a href="https://www.sprintcube.com" target="_blank">SprintCube</a></strong><br>
-                    The source code is released under the <a href="https://github.com/sprintcube/docker-compose-lamp/blob/master/LICENSE" target="_blank">MIT license</a>.
-                </p>
-            </div>
-        </footer>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pedir Cita - Pandora FMS</title>
+    <link rel="stylesheet" href="views/styles/style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="views/js/validate.js"></script>
+</head>
+<body>
+    
+    <div>
+        <div class="nav">
+            <ul>
+                <li><a href="exercise1">Ejercicio 1</a></li>
+                <li><a href="#" class="active">Ejercicio 2</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="content">
+        <div>
+            <form action="" method="POST" required class="appointment" id="appointment">
+                <input type="text" name="name" id="name" required placeholder="Introduce tu nombre" maxlength="32">
+                <input type="text" name="dni" id="dni" required pattern="[0-9]{8}[A-Za-z]{1}" placeholder="Introduce tu DNI">
+                <input type="number" name="phone" id="phone" required placeholder="Introduce tu teléfono" maxlength="9" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);">
+                <input type="email" name="email" id="email" required placeholder="Introduce tu correo electrónico">
+                <select name="type" id="type" >
+                    <option value="" class="disabled" disabled selected>Tipo de cita</option>
+                </select>
+                <button type="submit" name="send">Enviar</button>
+            </form>
+        </div>
+    </div>
+</body>
 </html>
